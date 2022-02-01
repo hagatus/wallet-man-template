8b06018de4a77f804c3862bf75cb51463459ed45
Anderson Contreira <anderson.contreira@gmail.com>
2022-02-01 11:27:42 -0300

    adicao de mustache para construção dinamica de templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,45 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-var rename = require('gulp-rename');
-var browserSync = require('browser-sync').create();
-var cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const mustache = require("gulp-mustache");
+/**
+ * TODO fazer todos os comandos enviarem os arquivos para a pasta build
+ */
+/**
+ * Mustache
+ */
+gulp.task('mustache', function () {
+    return gulp.src("src/*.mustache")
+            .pipe(mustache('src/dataView.json', {}, {}))
+            .pipe(rename({extname: '.html'}))
+            .pipe(gulp.dest("./build"));
+});
+/**
+ * Build
+ */
+gulp.task('build:css', function () {
+    return gulp.src("css/**").pipe(gulp.dest("./build/css"));
+});
+gulp.task('build:js', function () {
+    return gulp.src("js/**").pipe(gulp.dest("./build/js"));
+});
+gulp.task('build:images', function () {
+    return gulp.src("images/**").pipe(gulp.dest("./build/images"));
+});
 
+gulp.task('build', gulp.series('build:css', 'build:js', 'build:images', 'mustache'));
 /**
  * Sass
  */
 gulp.task('compile:sass', function () {
     return gulp.src('assets/layout/layout.scss')
-            .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-            .pipe(rename({ suffix: '.' + require('./package.json').version}))
+            .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+            .pipe(rename({suffix: '.' + require('./package.json').version}))
             .pipe(gulp.dest('./css'))
             .pipe(cleanCSS())
-            .pipe(rename({ suffix: '.min'}))
+            .pipe(rename({suffix: '.min'}))
             .pipe(gulp.dest('./css'));
 });
 
@@ -22,10 +48,10 @@ gulp.task('compile', gulp.series('compile:sass'));
 /**
  * Bootstrap
  */
-gulp.task('bootstrap:css', function() {
+gulp.task('bootstrap:css', function () {
     return gulp.src('node_modules/bootstrap/dist/css/**').pipe(gulp.dest('css/bootstrap'));
 })
-gulp.task('bootstrap:js', function() {
+gulp.task('bootstrap:js', function () {
     return gulp.src('node_modules/bootstrap/dist/js/**').pipe(gulp.dest('js/bootstrap'));
 })
 
@@ -34,10 +60,10 @@ gulp.task('bootstrap', gulp.series('bootstrap:css', 'bootstrap:js'))
 /**
  * Fontawesome
  */
-gulp.task('fontawesome:css', function() {
+gulp.task('fontawesome:css', function () {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/css/**').pipe(gulp.dest('css/fontawesome'));
 })
-gulp.task('fontawesome:js', function() {
+gulp.task('fontawesome:js', function () {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/js/**').pipe(gulp.dest('js/fontawesome'));
 })
 
@@ -47,20 +73,25 @@ gulp.task('fontawesome', gulp.series('fontawesome:css', 'fontawesome:js'))
  * Watch
  */
 gulp.task('watch', function () {
-    return gulp.watch('assets/layout/**/*.scss', gulp.series('compile:sass'));
+    gulp.watch('assets/layout/**/*.scss', gulp.series('compile:sass', 'mustache'));
+    gulp.watch('src/**/*.mustache', gulp.series('build'));
 });
 
 /**
  * Server
  */
 gulp.task('server', gulp.series('watch'), function () {
-    return browserSync.init({ server: '.' });
+    return browserSync.init({server: '.'});
 });
+
+/**
+ * Dist
+ */
 
 /**
  * Initialize Project
  */
-gulp.task('init', gulp.series('bootstrap', 'fontawesome', 'compile'))
+gulp.task('init', gulp.series('bootstrap', 'fontawesome', 'compile', 'mustache', 'build'))
 
 gulp.task('default', gulp.series('server'));
 
